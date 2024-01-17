f5d7b0c639b860a17464a7ea5a8b60d1fdf2abf1
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-17 12:57:11 -0300

    feat: criando a logica do while para o programa pergunta o número até o usuario acerta

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,11 +2,20 @@
 
 let numeroSecreto = 2;
 
-let chute = prompt('Digite o número entre 1 e 10');
+let chute;
 
-if(numeroSecreto == chute){
-    console.log("Você acerto o número secreto: " + numeroSecreto);
-}else{
-    alert("Você errou :(")
+
+while (chute != numeroSecreto){
+    chute = prompt('Digite o número entre 1 e 10');
+    if(numeroSecreto == chute){
+        console.log("Você acerto o número secreto: " + numeroSecreto);
+    }else{
+        if(chute > numeroSecreto){
+            alert("O número secreto é menor que " + chute)
+        }else{
+           alert("O número secreto é maior que " + chute) 
+        }
+    }
+    
 }
 
