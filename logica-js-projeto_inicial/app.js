ab2c9bf34bee21cdbb4a264dff00b3a7a7168805
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-17 12:16:26 -0300

    feat: começando a logica do jogo

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+alert('Boas vinda ao jogo do número secreto');
+
+let numeroSecreto = 29;
+
+let chute = prompt('Digite o número entre 1 e 30');
+
+if(numeroSecreto == chute){
+    console.log("Você acerto o número secreto!");
+}
