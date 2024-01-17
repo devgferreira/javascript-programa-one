734d6945a398d8c0f1213b823b08821f2e0974b5
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-17 13:01:45 -0300

    feat: adicionado o número de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,18 +4,23 @@ let numeroSecreto = 2;
 
 let chute;
 
+let tentativas = 1;
+
 
 while (chute != numeroSecreto){
     chute = prompt('Digite o número entre 1 e 10');
+    
     if(numeroSecreto == chute){
-        console.log("Você acerto o número secreto: " + numeroSecreto);
+        alert("Isso ai! Você descobriu o número secreto " + numeroSecreto + ", e tentou " + tentativas)
     }else{
+        tentativas++;
         if(chute > numeroSecreto){
-            alert("O número secreto é menor que " + chute)
+            alert("O número secreto é menor que " + chute);
         }else{
-           alert("O número secreto é maior que " + chute) 
+           alert("O número secreto é maior que " + chute);
         }
     }
-    
+
+   
 }
 
