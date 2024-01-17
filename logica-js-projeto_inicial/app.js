ae37d364fc6e38a5c1647eb217d28df7c891bb54
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-17 12:30:37 -0300

    feat: criando um simples prompt para pedir as informações do usuario

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,4 +1,4 @@
-alert('Boas vinda ao jogo do número secreto');
+/* alert('Boas vinda ao jogo do número secreto');
 
 let numeroSecreto = 29;
 
@@ -6,4 +6,19 @@ let chute = prompt('Digite o número entre 1 e 30');
 
 if(numeroSecreto == chute){
     console.log("Você acerto o número secreto!");
+}
+*/
+
+alert('Boas vindas ao nosso site!');
+let nome = "Lua";
+let idade = prompt("Digite seu idade");
+let numeroDeVendas = prompt("Digite suas venda");
+let saldoDisponivel = prompt("Digite seu saldo");
+
+if(idade == "" || nome == "" || numeroDeVendas == "" || saldoDisponivel == ""){
+    alert("Erro! Preencha todos os campos. ")
+}
+
+if(idade >= 25){
+    alert("Pode tirar carta")
 }
