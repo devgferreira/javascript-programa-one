0e3d2b003152792ce96b8e0898762b2cb071ac6e
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-17 12:48:36 -0300

    feat: criando o exercicio 2

---
+++ logica-js-projeto_inicial/exercicios 2.js
@@ -0,0 +1,9 @@
+// pedir informações para o usuario!
+alert('Que dia da semana é?');
+let dia = prompt("Digite o dia da semana");
+
+if(dia == "Sabadou"|| dia == "Domingo"){
+    alert("Bom final de semana");
+}else{
+    alert("Boa semana");
+}
