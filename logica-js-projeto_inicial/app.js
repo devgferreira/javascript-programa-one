6ff16951f8aba020a48793db89a486bf77089564
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-01-17 12:38:19 -0300

    feat: tratando o erro caso o usuario não acerte o número secreto

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,12 @@
  alert('Boas vinda ao jogo do número secreto');
 
-let numeroSecreto = 29;
+let numeroSecreto = 2;
 
-let chute = prompt('Digite o número entre 1 e 30');
+let chute = prompt('Digite o número entre 1 e 10');
 
 if(numeroSecreto == chute){
-    console.log("Você acerto o número secreto!");
+    console.log("Você acerto o número secreto: " + numeroSecreto);
+}else{
+    alert("Você errou :(")
 }
 
